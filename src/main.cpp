108255ecb1c418a935e86293b1f772dfd521613d
Norbert Bielak <norbertbielak@gmail.com>
2020-10-31 22:43:36 +0100

    Working input

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,6 @@ String numberServerOutput;
 namespace DefaultValues {
   const char* Color PROGMEM = "#333333";
   const bool BooleanValue PROGMEM = false;
-
 }
 
 namespace ErrorMessages{
@@ -70,10 +69,12 @@ namespace DataType{
 
 namespace JsonKey {
   const char* Title PROGMEM = "title";
+  const char* Body PROGMEM = "body";
   const char* Elements PROGMEM = "elements";
 
-
   const char* Name PROGMEM = "name";
+  const char* Width PROGMEM = "width";
+  const char* Height PROGMEM = "height";
   const char* PosX PROGMEM = "posX";
   const char* PosY PROGMEM = "posY";
   const char* DataType PROGMEM = "dataType";
@@ -92,15 +93,19 @@ namespace Website {
 
       // ** THIS METHOD USES COMMON MEMORY(DOCUMENT) FOR STORING JSON TO AVOID MULTIPLE HEAP ALLOCATIONS **
       // ** WHEN YOU GET OBJECT, THE PREVIOUS ONE IS DELETED AUTOMATICALLY **
-    virtual JsonObject toOutputJson() = 0;
-
+    virtual JsonObject toVisuinoJson() = 0;
+    virtual JsonObject toWebsiteJson() = 0;
     bool isInitializedOK() const {return initializedOK;}
     const String& getName() const  {return name;}
+
   protected:
     static DynamicJsonDocument jsonMemory;
     bool initializedOK;
+    uint16_t width;
+    uint16_t height;
     uint16_t posX;
     uint16_t posY;
+    uint8_t desktopScale;
     String name;
     String dataType;
   };
@@ -113,6 +118,18 @@ namespace Website {
       this->name = inputObject[JsonKey::Name].as<String>();
     } else {
       Serial.println("Name not found");
+      initializedOK = false;
+    }
+    if(inputObject.containsKey(JsonKey::Width)){
+      this->width = inputObject[JsonKey::Width];
+    } else {
+      Serial.println("Width not found");
+      initializedOK = false;
+    }
+    if(inputObject.containsKey(JsonKey::Height)){
+      this->height = inputObject[JsonKey::Height];
+    } else {
+      Serial.println("Height not found");
       initializedOK = false;
     }
     if(inputObject.containsKey(JsonKey::PosX)) {
@@ -159,13 +176,29 @@ namespace Website {
       }
     }
 
-    JsonObject toOutputJson() override {
+    JsonObject toVisuinoJson() override {
       JsonObject outputObj = jsonMemory.to<JsonObject>();
       outputObj[JsonKey::Name] = this->name;
       outputObj[JsonKey::DataType] = this->dataType;
       outputObj[JsonKey::Value] = this->value;
       return outputObj;
     }
+
+    JsonObject toWebsiteJson() override {
+      JsonObject websiteObj = jsonMemory.to<JsonObject>();
+
+      websiteObj[JsonKey::Name] = this->name;
+      websiteObj[JsonKey::DataType] = this->dataType;
+      websiteObj[JsonKey::Width] = this->width;
+      websiteObj[JsonKey::Height] = this->height;
+      websiteObj[JsonKey::PosX] = this->posX;
+      websiteObj[JsonKey::PosY] = this->posY;
+      websiteObj[JsonKey::Value] = this->value;
+      websiteObj[JsonKey::Color] = this->color;
+      websiteObj[JsonKey::ComponentType] = ComponentType::Input::Switch;
+      return websiteObj;
+    }
+
     void setValue(bool newValue) {this->value = newValue;}
     bool getValue() const {return this->value;}
   private:
@@ -177,41 +210,65 @@ namespace Website {
   public:
     Card() = default;
     ~Card() {this->garbageCollect();}
-
     enum class ComponentStatus : uint8_t{
       OK,
-      ALREADY_EXISTS,
       OBJECT_NOT_VALID,
     };
 
-
+    static void setJsonMemory(DynamicJsonDocument* mem);
     ComponentStatus add(JsonObject object);
+    JsonObject onHTTPRequest();
     void reserve(size_t size);
     void garbageCollect();
+    const std::vector<WebsiteComponent*>& getComponents() const {return components;}
   private:
-    static std::unique_ptr<DynamicJsonDocument> jsonMemory;
+    static DynamicJsonDocument* jsonMemory;
+    static bool isMemoryInitialized;
+    WebsiteComponent* getComponentByName(const char* name);
     bool componentAlreadyExists(const char* componentName);
     std::vector<WebsiteComponent*> components;
   };
 
+  DynamicJsonDocument* Card::jsonMemory = nullptr;
+  bool Card::isMemoryInitialized = false;
+
   Card::ComponentStatus Card::add(JsonObject object) {
     if(!object.containsKey(JsonKey::Name) || !object.containsKey(JsonKey::ComponentType)) return ComponentStatus::OBJECT_NOT_VALID;
-    if(componentAlreadyExists(object["name"])) return ComponentStatus::ALREADY_EXISTS;
-    WebsiteComponent* component;
     String componentType = object[JsonKey::ComponentType];
-
     using namespace ComponentType;
-    if(componentType.equals(Input::Switch)) components.push_back(new Switch(object));
+      // TODO: abstract it
+    if(componentType.equals(Input::Switch)) {
+      WebsiteComponent* component = new Switch(object);
+      Serial.println(componentType);
+      Serial.println(component->getName());
+      if(component->isInitializedOK()) components.push_back(component);
+      else {
+        delete component;
+        return ComponentStatus::OBJECT_NOT_VALID;
+      }
+    }
     //else if(componentType.equals(Input::Slider)) components.push_back(new Slider(object));
-
     return ComponentStatus::OK;
+  }
+
+  JsonObject Card::onHTTPRequest() {
+    if(isMemoryInitialized){
+      jsonMemory->clear();
+      JsonObject object = jsonMemory->to<JsonObject>();
+      JsonArray elements = object[JsonKey::Body].createNestedArray(JsonKey::Elements);
+      for(auto component : this->components) {
+        elements.add(component->toWebsiteJson());
+      }
+      return object;
+    } else{
+      Serial.println("not initialized");
+      return {};
+    }
   }
 
   bool Card::componentAlreadyExists(const char* componentName) {
     bool res = false;
-    for(const auto component : this->components) {
-      if(component->getName().equals(componentName)) res = true;
-    }
+    if(getComponentByName(componentName) != nullptr) res = true;
     return res;
   }
 
@@ -226,14 +283,19 @@ namespace Website {
   }
 
 
+  WebsiteComponent* Card::getComponentByName(const char *name) {
+    for(auto component : this->components){
+      if(component->getName().equals(name)) return component;
+    }
+    return nullptr;
+  }
+
+  void Card::setJsonMemory(DynamicJsonDocument *mem) {
+    jsonMemory = mem;
+    isMemoryInitialized = true;
+  }
+
 }
-
-
-
-std::vector<Website::WebsiteComponent*> components;
-
-
-
 
 
 const String testWebsiteConfigStr = {R"(
@@ -243,22 +305,83 @@ const String testWebsiteConfigStr = {R"(
     {
       "name" : "Lamp2",
       "dataType" : "boolean",
+      "width" : 20,
+      "height" : 10,
       "componentType" : "switch",
       "posX" : 20,
       "posY" : 40,
       "color" : "#ffeefe",
       "value": false
+    },
+    {
+      "name" : "Motor",
+      "dataType" : "boolean",
+      "width" : 21,
+      "height" : 15,
+      "componentType" : "switch",
+      "posX" : 250,
+      "posY" : 300,
+      "color" : "#abcdef",
+      "value": false
+    },
+    {
+      "name" : "Nothing",
+      "dataType" : "boolean",
+      "width" : 22,
+      "height" : 11,
+      "componentType" : "switch",
+      "posX" : 200,
+      "posY" : 150,
+      "color" : "#aaaaaa",
+      "value": false
+    },
+    {
+      "name" : "Something",
+      "dataType" : "boolean",
+      "width" : 27,
+      "height" : 14,
+      "componentType" : "switch",
+      "posX" : 200,
+      "posY" : 350,
+      "color" : "#bbaaaa",
+      "value": true
+    },
+    {
+      "name" : "Servo",
+      "dataType" : "boolean",
+      "width" : 18,
+      "height" : 11,
+      "componentType" : "switch",
+      "posX" : 200,
+      "posY" : 321,
+      "color" : "#abcada",
+      "value": true
+    },
+    {
+      "name" : "LightBulb",
+      "dataType" : "boolean",
+      "width" : 32,
+      "height" : 29,
+      "componentType" : "switch",
+      "posX" : 232,
+      "posY" : 321,
+      "color" : "#abcgdb",
+      "value": true
     }
   ]
 })"};
 
 
 
+Website::Card card;
+
 namespace JsonReader {
   size_t documentSize = 0;
+  DynamicJsonDocument* websiteData;
 
   enum class InputJsonStatus : uint8_t {
     OK,
+    JSON_OVERFLOW,
     ALLOC_ERROR,
     TITLE_NOT_FOUND,
     ELEMENTS_NOT_FOUND,
@@ -266,20 +389,18 @@ namespace JsonReader {
     NAME_NOT_FOUND
   };
 
-
-  InputJsonStatus createComponent(JsonObject object, std::vector<Website::WebsiteComponent*>& elements) {
-    if( !object.containsKey(JsonKey::Name) ) return InputJsonStatus::NAME_NOT_FOUND;
-  }
-
-
-
-
   InputJsonStatus readWebsiteComponentsFromJson(const String& json){
-    static DynamicJsonDocument inputData(JSON_OBJECT_SIZE(json.length()));
-    static bool isVectorReserved = false;
+    static DynamicJsonDocument inputData(json.length());
+    websiteData = new DynamicJsonDocument(json.length());
     if(inputData.capacity() == 0) return InputJsonStatus::ALLOC_ERROR;
+    uint16 size = inputData.capacity();
+    Serial.println(size);
+    Serial.println(json.length());
+    Website::Card::setJsonMemory(websiteData);
+    static bool isVectorReserved = false;
     documentSize = inputData.capacity();
     deserializeJson(inputData, json);
+    if(inputData.overflowed()) return InputJsonStatus::JSON_OVERFLOW;
     JsonObject inputObject = inputData.as<JsonObject>();
     if(!inputObject.containsKey(JsonKey::Title)) return InputJsonStatus::TITLE_NOT_FOUND;
     if(!inputObject.containsKey(JsonKey::Elements)) return InputJsonStatus::ELEMENTS_NOT_FOUND;
@@ -287,21 +408,18 @@ namespace JsonReader {
     JsonArray elements = inputObject[JsonKey::Elements].as<JsonArray>();
 
     if( !isVectorReserved ) {
-      components.reserve(elements.size());
-      isVectorReserved = true;
+      card.reserve(elements.size());
     }
 
-    Website::WebsiteComponent* componentPtr;
-    for(const JsonObject element : elements){
-      componentPtr = new Website::Switch(element);
-      if(componentPtr->isInitializedOK()) components.push_back(componentPtr);
-      else {
-        serializeJsonPretty(element, Serial);
-        Serial.println("error");
-        delete componentPtr;
-        break;
+    using Website::Card;
+    for(JsonObject element : elements){
+      if(card.add(element) != Card::ComponentStatus::OK){
+        return InputJsonStatus::ALLOC_ERROR;
       }
     }
+
+
+
     return InputJsonStatus::OK;
   }
   void inputJsonParse_ErrorHandler(InputJsonStatus status){
@@ -322,10 +440,6 @@ namespace JsonReader {
   }
 }
 
-inline void componentsGarbageCollect(){
-  for (auto component : components) delete component;
-  delete JsonReader::inputData;
-}
 
 
 
@@ -394,7 +508,7 @@ void HTTPSetMappings(AsyncWebServer& webServer){
 
 
   webServer.on("/input", HTTP_GET, [] (AsyncWebServerRequest* request){
-    AsyncWebServerResponse* response = request->beginResponse(HTTP_STATUS_OK, "application/json", JsonReader::inputData->as<String>());
+    AsyncWebServerResponse* response = request->beginResponse(HTTP_STATUS_OK, "application/json", card.onHTTPRequest()[JsonKey::Body]);
     fullCorsAllow(response);
     request->send(response);
   });
@@ -430,14 +544,7 @@ void setup(){
 
   JsonReader::InputJsonStatus status = JsonReader::readWebsiteComponentsFromJson(testWebsiteConfigStr);
   if(status != JsonReader::InputJsonStatus::OK) Serial.println("Some error xd");
-
-  if(!components.empty()){
-    JsonObject obj = components[0]->toOutputJson();
-    Serial.println("Created");
-    serializeJsonPretty(obj, Serial);
-  } else {
-    Serial.println("Json not initializated");
-  }
+  if(status == JsonReader::InputJsonStatus::ALLOC_ERROR) Serial.println("Zjebales xd");
 
 }
 
